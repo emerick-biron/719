9063c7f7bb5dc69902d9ca666c703136d3fc983a
emerick-biron <emerick.biron@gmail.com>
2024-02-06 10:34:37 +0100

    data validation

--- septunneuf-serveur/septunneuf-incubators/src/main/kotlin/fr/ebiron/septunneuf/incubators/dto/CreateIncubatorRequest.kt
+++ septunneuf-serveur/septunneuf-incubators/src/main/kotlin/fr/ebiron/septunneuf/incubators/dto/CreateIncubatorRequest.kt
@@ -3,6 +3,6 @@ package fr.ebiron.septunneuf.incubators.dto
 import jakarta.validation.constraints.NotBlank
 
 data class CreateIncubatorRequest(
-    @NotBlank(message = "heroName is mandatory")
+    @field:NotBlank(message = "heroName is mandatory")
     val heroName: String
 )
