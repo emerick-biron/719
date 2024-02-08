12330744f12f72d6db7e871efe086976f4dae1b3
emerick-biron <emerick.biron@gmail.com>
2024-02-08 11:14:20 +0100

    cors management

---
+++ septunneuf-serveur/septunneuf-incubators/src/main/kotlin/fr/ebiron/septunneuf/incubators/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package fr.ebiron.septunneuf.incubators.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                if (System.getenv("NODE_ENV")?.equals("DEVELOPMENT") == true) {
+                    registry.addMapping("/**").allowedOrigins("*");
+                }
+            }
+        }
+    }
+}
