5e105b7b43cf39f162287a016762e63af74c1998
emerick-biron <emerick.biron@gmail.com>
2024-02-19 20:56:18 +0100

    Better way to handle cors activation/deactivation

--- septunneuf-serveur/septunneuf-incubators/src/main/kotlin/fr/ebiron/septunneuf/incubators/config/WebConfig.kt
+++ septunneuf-serveur/septunneuf-incubators/src/main/kotlin/fr/ebiron/septunneuf/incubators/config/WebConfig.kt
@@ -1,5 +1,7 @@
 package fr.ebiron.septunneuf.incubators.config
 
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -7,14 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-class WebConfig {
+class WebConfig(
+    @Value("#{environment['DISABLE_CORS']?:false}")
+    private val disableCors: Boolean
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                if (System.getenv("NODE_ENV")?.equals("DEVELOPMENT") == true) {
-                    registry.addMapping("/**").allowedOrigins("*");
+                log.info("Cors disabled: $disableCors")
+                if (disableCors) {
+                    registry.addMapping("/**").allowedOrigins("*")
                 }
             }
         }
--- septunneuf-serveur/septunneuf-monsters/src/main/java/fr/ebiron/septunneuf/monsters/config/WebConfig.java
+++ septunneuf-serveur/septunneuf-monsters/src/main/java/fr/ebiron/septunneuf/monsters/config/WebConfig.java
@@ -1,5 +1,9 @@
 package fr.ebiron.septunneuf.monsters.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +11,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    private final boolean disableCors;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @Autowired
+    public WebConfig(@Value("#{environment['DISABLE_CORS']?:false}") boolean disableCors) {
+        this.disableCors = disableCors;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                String nodeEnv = System.getenv("NODE_ENV");
-                if (nodeEnv != null && nodeEnv.equals("DEVELOPMENT")) {
+                log.info("Cors disabled: {}", disableCors);
+                if (disableCors) {
                     registry.addMapping("/**").allowedOrigins("*");
                 }
             }
